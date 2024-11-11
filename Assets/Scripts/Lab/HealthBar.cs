5dc529b8c8ea8054e8a36c806481ecfc795a1168
LAPT0PZER <songkrod.tong@bumail.net>
2024-11-12 01:14:16 +0700

    HealthBar Script

---
+++ Assets/Scripts/Lab/HealthBar.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] Slider healthSlider;
+    public void SetMaxHealth(int health)
+    {
+        healthSlider.maxValue = health;
+        healthSlider.value = health;
+
+        Debug.Log($"HealthBar slider: {healthSlider.value} / {healthSlider.maxValue}");
+    }
+
+    public void UpdateHealthBar(int health)
+    {
+        healthSlider.value = health;
+    }
+}
